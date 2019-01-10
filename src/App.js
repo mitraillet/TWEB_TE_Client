f0ae3362e4b28c142d22e303e293f1da83899304
mitraillet <steve.henriquet@gmail.com>
2019-01-10 16:02:31 +0100

    non fini

--- src/App.js
+++ src/App.js
@@ -1,77 +1,9 @@
-import React, { useState } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import { AuthContext } from './AuthProvider';
-
-const HomePage = () => (
-  <AuthContext>
-    {({ signOut }) => (
-      <div>
-        <h1>Welcome !</h1>
-        <button onClick={signOut}>LOGOUT</button>
-      </div>
-    )}
-  </AuthContext>
-);
-
-const LoginPage = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
-  return (
-    <AuthContext>
-      {({ error, user, signIn }) => {
-
-        if (user) {
-          return <Redirect to="/" />;
-        }
-
-        const onSubmit = (e) => {
-          e.preventDefault();
-          signIn({ username, password });
-        };
-
-        return (
-          <div>
-            <h1>Login</h1>
-            <form onSubmit={onSubmit}>
-              <input
-                type="text"
-                placeholder="username"
-                value={username}
-                onChange={e => setUsername(e.target.value)}
-              />
-              <br />
-              <input
-                type="password"
-                placeholder="password"
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-              />
-              <br />
-              <button type="submit">LOGIN</button>
-              <p style={{ color: 'red' }}>{error}</p>
-            </form>
-          </div>
-        )
-      }}
-    </AuthContext>
-  )
-}
-
-const ProtectedRoute = ({ component: Component, ...rest }) => (
-  <Route {...rest} render={(params) => (
-    <AuthContext>
-      {({ user }) => user
-        ? <Component {...params} />
-        : <Redirect to="/login" />}
-    </AuthContext>
-  )}
-  />
-)
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import HomePage from './HomePage';
 
 export default () => (
   <Switch>
-    <ProtectedRoute path="/" exact component={HomePage} />
-    <Route path="/login" component={LoginPage} />
+    <Route path="/" exact component={HomePage} />
   </Switch>
 );
---
+++ src/HomePage.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+  state = {
+    films: null,
+    filmsSelectedId: []
+  }
+
+
+  handleOnClick = (e) => {
+    this.state.films.filter(movie => {
+      if (e.target.id == movie.id) {
+        let filmsSelectedIds = this.state.filmsSelectedId;
+        if(filmsSelectedIds.indexOf(movie.id) != -1){
+          filmsSelectedIds.splice(filmsSelectedIds.indexOf(movie.id), 1)
+        }
+        else {
+          filmsSelectedIds.push(movie.id);
+        }
+        this.setState({filmsSelectedId : filmsSelectedIds });
+        console.log(this.state.filmsSelectedId)
+        return null;
+      }
+    })
+  }
+  
+  componentDidMount() {
+    fetch('https://api.themoviedb.org/3/movie/popular?page=1&api_key=f1be4bafe6f7cb0cb84f5948c5b75497')
+      .then( results => {
+        return results.json();
+      }).then(data => {
+        this.setState({
+          films: data.results
+        })
+
+      })
+  }
+
+  render() {
+    return (
+      this.state.films ?
+      this.state.films.map(movie => {
+          return(
+            <div className="movies" key={movie.id} >
+              <h1 className="titleMovies" >{movie.title}</h1>
+              <img className="imgMovie" id={movie.id} src={'https://image.tmdb.org/t/p/w500/'+ movie.poster_path} alt={"Poster of" + movie.title} onClick={this.handleOnClick}></img>
+        </div>
+        )
+      }) :
+      <span>Loading...</span>
+    )
+  }
+}
+/* const HomePage = () => (
+  <AuthContext>
+    {({ signOut }) => (
+      <div>
+        <header>
+          Films
+        </header>
+        <div className="content">
+
+        </div>
+      </div>
+    )}
+  </AuthContext>
+);*/
+
+export default HomePage;
